d70b0332c16d90d00dd22af484a21ed281c6269a
Muhammad Farid Saleh <github.farid1@gmail.com>
2023-11-16 17:15:11 +0900

    update scripts

--- apps/fm_pjpa/views.py
+++ apps/fm_pjpa/views.py
@@ -240,9 +240,9 @@ def build_breadcrumbs(url):
 
 def showfolder(request, slug, year):
     if request.method == 'POST':
-        if request.FILES and request.FILES['uploadfiles']:
-            upload = request.FILES.getlist('uploadfiles')
-            return HttpResponse(upload[0])
+        if request.FILES:
+            uploads = request.FILES.getlist('uploadfiles')
+            return HttpResponse(str(uploads[1]))
         else:            
             path = os.path.join(settings.FM_LOCATION, __package__.split('.')[1], slug, year, request.POST['folder'], request.POST['filename'])
             if os.path.exists(path):
