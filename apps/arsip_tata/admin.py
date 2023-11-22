265e1156a735fe8de7ded4b0242ff81374f05bce
Muhammad Farid Saleh <github.farid1@gmail.com>
2023-11-23 08:46:33 +0900

    update scripts

--- apps/arsip_tata/admin.py
+++ apps/arsip_tata/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Year)
+admin.site.register(models.Box)
+admin.site.register(models.Bundle)
+admin.site.register(models.Item)
