7e50be61d0527d786b15bdab46dc0dd4cffc5b26
Mohamad Farid Saleh <github.farid1@gmail.com>
2025-01-25 21:03:02 +0900

    Update Scripts

--- apps/arsip_tata/views.py
+++ apps/arsip_tata/views.py
@@ -1484,8 +1484,8 @@ def box_sync(request, pk):
         bundles = Bundle.objects.filter(box=prevbox)
         bundle_maxnumber = bundles.aggregate(Max("bundle_number"))['bundle_number__max']
         # breakpoint()
-        bundle_maxid = bundles.aggregate(Max("id"))['id__max']
-        # bundle_maxid = Bundle.objects.get(bundle_number=bundle_maxnumber, yeardate=prevbox.yeardate).id
+        # bundle_maxid = bundles.aggregate(Max("id"))['id__max']
+        bundle_maxid = Bundle.objects.get(bundle_number=bundle_maxnumber, yeardate=prevbox.yeardate).id
         item_maxnumber = Item.objects.filter(bundle_id=bundle_maxid).aggregate(Max("item_number"))['item_number__max']
         # raise (bundle_maxnumber, item_maxnumber)
         bundle_number = bundle_maxnumber+1
