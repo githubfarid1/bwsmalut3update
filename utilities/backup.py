41bd5e3ccb47944edbdd01a7095aabdecceae1a2
Mohamad Farid Saleh <github.farid1@gmail.com>
2025-09-12 16:53:57 +0900

    update scripts

--- utilities/backup.py
+++ utilities/backup.py
@@ -4,7 +4,7 @@ from alihmedia_inactive.settings import *
 import datetime
 import glob
 import uuid
-FILE_COUNT = 14
+FILE_COUNT = 7
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
